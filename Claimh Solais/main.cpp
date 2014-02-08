7a9e46f130036f7604d98ac203c013ed2b8428a2
Damian <damiansito93@gmail.com>
2014-02-07 22:08:45 -0500

    Initial Commit

---
+++ Claimh Solais/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Claimh Solais
+//
+//  Created by Damian Lajara on 2/7/14.
+//  Copyright (c) 2014 Damian Lajara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
